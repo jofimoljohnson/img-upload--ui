3af0824be3079f70dd0d8995f40c7b859106fdb8
JYOTHI JOHNSON <jyothijohnson1999@gmail.com>
2023-01-27 10:46:56 +0530

    img upload commit

---
+++ src/components/Home.js
@@ -0,0 +1,112 @@
+// import axios from "axios";
+import React, { useState } from "react";
+import "./Home.css";
+
+// function Home() {
+  
+//   const [image,setImage] = useState('')
+
+//   function handleImage(e){
+//     console.log(e.target.files);
+//      setImage(e.target.files[0])
+//   }
+
+//   function handleApi(){
+//     const formData = new formData()
+//     formData.append('image',image)
+//     axios.post('url',formData).then((res)=>{
+//       console.log(res);
+//     })
+//   }
+
+//   return (
+//     <div>
+//       <input type="file" name='file' onChange={handleImage}/>
+//       <button>Submit</button>
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+
+const Home = () => {
+  const [selectedImages, setSelectedImages] = useState([]);
+
+  const onSelectFile = (event) => {
+    const selectedFiles = event.target.files;
+    const selectedFilesArray = Array.from(selectedFiles);
+
+    const imagesArray = selectedFilesArray.map((file) => {
+      return URL.createObjectURL(file);
+    });
+
+    setSelectedImages((previousImages) => previousImages.concat(imagesArray));
+
+    // FOR BUG IN CHROME
+    event.target.value = "";
+  };
+
+  function deleteHandler(image) {
+    setSelectedImages(selectedImages.filter((e) => e !== image));
+    URL.revokeObjectURL(image);
+  }
+
+  return (
+    <section>
+      <label className="labelbox">
+        + Add Images
+        <br />
+        <span>up to 10 images</span>
+        <input
+          type="file"
+          name="images"
+          onChange={onSelectFile}
+          multiple
+          accept="image/png , image/jpeg, image/webp"
+        />
+      </label>
+      <br />
+
+      <input type="file" multiple />
+
+      {selectedImages.length > 0 &&
+        (selectedImages.length > 10 ? (
+          <p className="error">
+            You can't upload more than 10 images! <br />
+            <span>
+              please delete <b> {selectedImages.length - 10} </b> of them{" "}
+            </span>
+          </p>
+        ) : (
+          <button
+            className="upload-btn"
+            onClick={() => {
+              console.log(selectedImages);
+            }}
+          >
+            UPLOAD {selectedImages.length} IMAGE
+            {selectedImages.length === 1 ? "" : "S"}
+          </button>
+        ))}
+
+      <div className="images">
+        {selectedImages &&
+          selectedImages.map((image, index) => {
+            return (
+              <div key={image} className="image">
+                <img src={image} height="200" alt="upload" />
+                <button onClick={() => deleteHandler(image)}>
+                  delete image
+                </button>
+                <p>{index + 1}</p>
+              </div>
+            );
+          })}
+      </div>
+    </section>
+  );
+
+}
+
+export default Home
